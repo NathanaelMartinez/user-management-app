71306d953aa38a68db93ba4ec2dc25f8314e6826
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-09-22 21:49:10 -0500

    configure CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: 'https://user-management-app-frontend.onrender.com',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Authorization', 'Content-Type'],
 }));
