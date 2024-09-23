9c80e1cef0089644d82039684972ffca8e006797
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-09-22 21:14:38 -0500

    change imports to js

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
